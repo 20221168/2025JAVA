86cede03cb399cc0fcd2cec57cfee39824b7e929
20221168 <kny6404@gmail.com>
2025-10-14 10:52:35 +0900

    2025/10/14 수업실습

--- Car.java
+++ Car.java
@@ -1,40 +1,5 @@
-package sec03.exam01;
+package sec04exam;
 
 public class Car {
-//	String company = "Kia";
-//	String model ="Truck";
-//	int maxSpeed = 150;
-	String company;
-	String model ;
-	int maxSpeed;
-	
-	public Car() {
-		this("Kia","truck",150);
-	}
-	
-	public Car(String company) {
-		//this.company = company;
-		this("Kia","truck",150);
-	}
-	public Car(String company, String model) {
-//		this.company = company;
-//		this.model = model;
-		this("Kia","truck",150);
-		
-	}
-	public Car(String company, String model,
-			int maxSpeed) 
-	{
-		this.company = company;
-		this.model = model;
-		this.maxSpeed = maxSpeed;
-	}
-	public void ShowInfo() {
-		System.out.println("company:"+ company);
-		System.out.println("Model:"+ model);
-		System.out.println("maxSpeed:"+maxSpeed);
-	}
-	
-	
 
 }
---
+++ Main.java
@@ -0,0 +1,10 @@
+package sec04exam;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
