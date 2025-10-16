1e728a2b8c1e71b13723dca93602345a49d6dc4b
20221168 <kny6404@gmail.com>
2025-10-16 14:47:23 +0900

    2025/10/16 수업 실습

--- Car.java
+++ Car.java
@@ -1,5 +1,15 @@
-package sec04exam;
+package sec01.exam01;
 
 public class Car {
+	int speed;
+	
+	Car(int speed){
+		this.speed = speed;
+	}
+	
+	void ShowSpeed() {
+		System.out.println("Car Speed:"
+				+speed);
+	}
 
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
-package sec04exam;
+package sec01.exam01;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Car car;
+		Truck truck = new Truck(80,200);
+		
+
+		truck.ShowSpeed();
+		car = truck;
+		car.ShowSpeed();
+		
+		System.out.println("car speed is"
+				+ car.speed);
+		System.out.println("truck speed is"
+				+ car.speed);
 
 	}
 
